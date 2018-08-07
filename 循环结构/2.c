806200eb3c517d4349cd45499a4b612333bed58c
ReWKing <baymaxing@gmail.com>
2018-08-07 22:32:23 +0800

    循环结构

---
+++ 循环结构/2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+  int n, i, j;
+  scanf("%d", &n);
+  for (j = 1;j <= n; j++)
+  {
+    for (i = 1; i <= n; i++)
+      printf("*");
+    printf("\n");
+  }
+}
---
+++ 循环结构/Fibonacci.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+  int f1, f2, i;
+  f1 = 1; f2 = 1;
+  for (i = 1; i <= 20; i++)
+  {
+    printf("%d%d\n", f1, f2);
+    f1 = f1 + f2;
+    f2 = f2 + f1;
+  }
+  printf("\n");
+}
