ccab4b6e1b65ac29785905f562ddbd35b6029e01
ReWKing <baymaxing@gmail.com>
2018-09-07 18:23:42 +0800

    update

--- 循环结构/Fibonacci.c
+++ 循环结构/Fibonacci.c
@@ -5,9 +5,12 @@ void main()
   f1 = 1; f2 = 1;
   for (i = 1; i <= 20; i++)
   {
-    printf("%d%d\n", f1, f2);
     f1 = f1 + f2;
     f2 = f2 + f1;
+    printf("%d%d\n", f1, f2);
   }
   printf("\n");
 }
+\[
+e ^ ()
+\]
